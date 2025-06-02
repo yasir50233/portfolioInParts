b2736c122f1b3938a3c75cc54a26b6e97a641375
yasir50233 <yasirkhan50233@gmail.com>
2025-06-02 19:53:43 +0500

    the picture component addded

---
+++ Img Box/src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+import Header from './Header';
+import Intro from './intro';
+import Imgbox from './imgbox';
+
+function App() {
+
+  return (
+    <><Header></Header>
+       <Intro></Intro>
+       <Imgbox></Imgbox>
+    </>
+  )
+}
+
+export default App;
---
+++ Img Box/src/intro.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css";
+import "./intro.css";
+
+const Intro = ()=>{
+    return (
+       <div className="intro-main-box">
+        <h1>
+            <h2><span>Yasir</span> Khan.</h2>
+             FRONT END
+            <span>
+            <span>DEVELOPER</span>
+            <span>CODER</span>
+            <span>DESIGNER</span>
+            <span>PROGRAMMER</span>
+            <span>FREELANCER</span>
+        </span>
+        </h1>
+        <button class="button-57" role="button"><span class="text">Contact</span><span></span></button>
+
+    </div>
+    );
+}
+export default Intro;
