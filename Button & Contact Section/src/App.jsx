fabc5b92ee59df57a86a2194e543aeb59fbdf955
yasir50233 <yasirkhan50233@gmail.com>
2025-06-03 14:51:34 +0500

    the btn and contact section

---
+++ Button & Contact Section/src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css';
+import Header from './Header';
+import Intro from './intro';
+import Imgbox from './imgbox';
+import Btn_And_contact from './btn and contact';
+
+function App() {
+
+  return (
+    <><Header></Header>
+       <Intro></Intro>
+       <Imgbox></Imgbox>
+       <Btn_And_contact></Btn_And_contact>
+        </>
+  )
+}
+
+export default App;
---
+++ Button & Contact Section/src/btn and contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import "./btn and contact.css";
+
+const Btn_And_contact = ()=>{
+    return (
+        <div className="btn-box">
+        <a href="https://web.whatsapp.com/" target="_blank"><h3><i class="fa-solid fa-square-phone" ></i> +92 313 9850233</h3></a>
+        <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=new" target="_blank"><h3><i class="fa-solid fa-envelope"></i> YasirKhan9850233@gmail.com</h3></a>
+        <button className="button-59" role="button">Contact Now</button>
+    </div>
+    );
+}
+
+export default Btn_And_contact;
